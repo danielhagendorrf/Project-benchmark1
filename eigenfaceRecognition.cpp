feadd35aa57aeb7de12126d7af1490986f2822e2
danielhagendorrf <daniel.hagendorf@gmail.com>
2016-08-18 12:34:40 +0300

    basic code
    source-paint around a face in a picture
    eigenface-eigenface recognizer code
    fisherface-fisherface recognizer code

---
+++ eigenfaceRecognition.cpp
@@ -0,0 +1,75 @@
+
+/*
+* Copyright (c) 2011. Philipp Wagner <bytefish[at]gmx[dot]de>.
+* Released to public domain under terms of the BSD Simplified license.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*   * Redistributions of source code must retain the above copyright
+*     notice, this list of conditions and the following disclaimer.
+*   * Redistributions in binary form must reproduce the above copyright
+*     notice, this list of conditions and the following disclaimer in the
+*     documentation and/or other materials provided with the distribution.
+*   * Neither the name of the organization nor the names of its contributors
+*     may be used to endorse or promote products derived from this software
+*     without specific prior written permission.
+*
+*   See <http://www.opensource.org/licenses/bsd-license>
+*/
+
+#include "opencv2/core.hpp"
+#include "opencv2/face.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace cv;
+using namespace cv::face;
+using namespace std;
+
+static void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, char separator = ';') {
+	std::ifstream file(filename.c_str(), ifstream::in);
+	if (!file) {
+		string error_message = "No valid input file was given, please check the given filename.";
+		CV_Error(Error::StsBadArg, error_message);
+	}
+	string line, path, classlabel;
+	while (getline(file, line)) {
+		stringstream liness(line);
+		getline(liness, path, separator);
+		getline(liness, classlabel);
+		if (!path.empty() && !classlabel.empty()) {
+			images.push_back(imread(path, 0));
+			labels.push_back(atoi(classlabel.c_str()));
+		}
+	}
+}
+
+int eigenface(int argc, const char *argv[]) {
+	string csv = string("c:/csv.csv");
+    vector<Mat> images;
+	vector<int> labels;
+	try {
+		read_csv(csv, images, labels);
+	}
+	catch (cv::Exception& e) {
+		cerr << "Error opening file \"" << csv << "\". Reason: " << e.msg << endl;
+		exit(1);
+	}
+	if (images.size() <= 1) {
+		string error_message = "This demo needs at least 2 images to work. Please add more images to your data set!";
+		CV_Error(Error::StsError, error_message);
+	}
+	Mat img = imread("c:/WeizmanProjectPictures/Daniel/p1.jpg", 0);
+	int test = 0;
+	Ptr<BasicFaceRecognizer> model = createEigenFaceRecognizer(0);
+	model->train(images, labels);
+	int predictedLabel = model->predict(img);
+	string result_message = format("Predicted class = %d / Actual class = %d.", predictedLabel, test);
+	cout << result_message << endl;
+	waitKey(0);
+	return 0;
+}
