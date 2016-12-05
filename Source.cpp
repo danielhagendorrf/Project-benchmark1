7f9cea4a78a865bdbf71ba86a8f6c41b76f6c7a9
danielhagendorrf <daniel.hagendorf@gmail.com>
2016-12-05 17:16:04 +0200

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -3,8 +3,12 @@
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 #include "C:/Users/Daniel Hagendorf/Documents/Visual Studio 2015/Projects/Project6/Project6/eigenfaceRecognition.h"
+#include "C:/Users/Daniel Hagendorf/Documents/Visual Studio 2015/Projects/Project6/Project6/LBPRecognition.h"
 #include "copyFace.h"
 #include "readCSV.h"
+#include "fisherface.h"
+#include "alignFace.h"
+#include "colorBalancing.h"
 
 #include <iostream>
 #include <fstream>
@@ -15,13 +19,17 @@ using namespace std;
 using namespace cv;
 
 
-/** Function Headers */
 void detectAndDisplay(Mat frame, vector<Mat>& images, vector<int>& labels);
-int eigen(Mat img, CascadeClassifier face_cascade, vector<Mat>& images, vector<int>& labels);
+int eigen(Mat img, CascadeClassifier face_cascade, Ptr<BasicFaceRecognizer> model);
+int LBP(Mat img, CascadeClassifier face_cascade, Ptr<FaceRecognizer> model);
+Ptr<FaceRecognizer> trainLBP(vector<Mat>& images, vector<int>& labels);
+Ptr<BasicFaceRecognizer> train(vector<Mat>& images, vector<int>& labels);
+int fisher(Mat img, CascadeClassifier face_cascade, vector<Mat>& images, vector<int>& labels);
 void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, char separator, CascadeClassifier face_cascade);
 Mat copyFace(Mat img, int leftWidth, int bottomHeight, int rightWidth, int topHeight);
+void alignFaceWithEyes(Mat img, int lEyeY, int rEyeY, int imgW, int imgH);
+void colorBalancing(Mat& img, Mat& rImg, float percent);
 
-/** Global variables */
 String face_cascade_name = "haarcascade_frontalface_alt.xml";
 String eyes_cascade_name = "haarcascade_eye_tree_eyeglasses.xml";
 CascadeClassifier face_cascade;
@@ -29,15 +37,15 @@ CascadeClassifier eyes_cascade;
 String window_name = "Capture - Face detection";
 
 
-/** @function main */
 int main(void)
 {
-	VideoCapture capture;
-	Mat frame;
 	Mat img;
-	img = imread("C:/Users/Daniel Hagendorf/Pictures/Camera Roll/p1.jpg");
-	
-	//-- 1. Load the cascades
+	img = imread("C:/Users/Daniel Hagendorf/Pictures/Camera Roll/p8.jpg");
+	//Size size(1280,720);
+	//if (img.size() != size)
+		//resize(img, img, size);
+	//cout << size.height << endl;
+	//cout << size.width << endl;
 	if (!face_cascade.load(face_cascade_name)) { printf("--(!)Error loading face cascade\n"); return -1; };
 	if (!eyes_cascade.load(eyes_cascade_name)) { printf("--(!)Error loading eyes cascade\n"); return -1; };
 
@@ -55,46 +63,51 @@ int main(void)
 		string error_message = "This demo needs at least 2 images to work. Please add more images to your data set!";
 		CV_Error(Error::StsError, error_message);
 	}
-	//-- 2. Read the video stream
 	detectAndDisplay(img,images,labels);
 	waitKey(0);
 	return 0;
 }
 
-/** @function detectAndDisplay */
 void detectAndDisplay(Mat frame, vector<Mat>& images, vector<int>& labels)
 {
+	Mat frame1;
+	colorBalancing(frame, frame1, 20.0f);
+	namedWindow("hhh", WINDOW_AUTOSIZE);
+	imshow("hhh", frame1);
 	std::vector<Rect> faces;
 	Mat frame_gray;
 
-	cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+	cvtColor(frame1, frame_gray, COLOR_BGR2GRAY);
 	equalizeHist(frame_gray, frame_gray);
 
-	//-- Detect faces
 	face_cascade.detectMultiScale(frame_gray, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
-
+	int prediction=0;
 	for (size_t i = 0; i < faces.size(); i++)
 	{
 		Point center(faces[i].x + faces[i].width / 2, faces[i].y + faces[i].height / 2);
-		ellipse(frame, center, Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
-		Mat face = copyFace(frame,faces[i].x, faces[i].y, faces[i].x + faces[i].width, faces[i].y + faces[i].height);
+		ellipse(frame1, center, Size(faces[i].width / 2, faces[i].height / 2), 0, 0, 360, Scalar(255, 0, 255), 4, 8, 0);
+		Mat faceROI = frame_gray(faces[i]);
+		Mat face = copyFace(frame1,faces[i].x, faces[i].y, faces[i].x + faces[i].width, faces[i].y + faces[i].height);
+		cvtColor(face, face,COLOR_BGR2GRAY);
 		namedWindow("Daniel", WINDOW_AUTOSIZE);
 		imshow("Daniel", face);
-		cvtColor(face, face,COLOR_BGR2GRAY);
-		eigen(face, face_cascade, images, labels);
-		Mat faceROI = frame_gray(faces[i]);
-		std::vector<Rect> eyes;
-
-		//-- In each face, detect eyes
-		/*eyes_cascade.detectMultiScale(faceROI, eyes, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
-
-		for (size_t j = 0; j < eyes.size(); j++)
-		{
-			Point eye_center(faces[i].x + eyes[j].x + eyes[j].width / 2, faces[i].y + eyes[j].y + eyes[j].height / 2);
-			int radius = cvRound((eyes[j].width + eyes[j].height)*0.25);
-			circle(frame, eye_center, radius, Scalar(255, 0, 0), 4, 8, 0);
-		}*/
+		Ptr<BasicFaceRecognizer> model = train(images, labels);
+		prediction=eigen(face, face_cascade,model);
+		if (prediction = 0) {
+			cout << "the man is in the database" << endl;
+		}
+		else {
+			cout << "not recognised" << endl;
+		}
+		Ptr<FaceRecognizer> model2 = trainLBP(images, labels);
+		prediction = LBP(face, face_cascade, model);
+		if (prediction = 0) {
+			cout << "the man is in the database" << endl;
+		}
+		else {
+			cout << "not recognised" << endl;
+		}
 	}
-	//-- Show what you got
 	imshow(window_name, frame);
 }
+
--- readCSV.cpp
+++ readCSV.cpp
@@ -3,6 +3,7 @@
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 #include "copyFace.h"
+#include "colorBalancing.h"
 
 #include <iostream>
 #include <fstream>
@@ -14,6 +15,7 @@ using namespace cv;
 
 
 void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, char separator, CascadeClassifier face_cascade) {
+	//Size size(1280, 720);
 	std::ifstream file(filename.c_str(), ifstream::in);
 	if (!file) {
 		string error_message = "No valid input file was given, please check the given filename.";
@@ -24,19 +26,46 @@ void read_csv(const string& filename, vector<Mat>& images, vector<int>& labels, 
 		stringstream liness(line);
 		getline(liness, path, separator);
 		getline(liness, classlabel);
+		int oneTime = 0;
 		if (!path.empty() && !classlabel.empty()) {
 			std::vector<Rect> faces;
 			Mat img = imread(path);
-			face_cascade.detectMultiScale(img, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
-			if (faces.size() >= 1) {
-				Mat face = copyFace(img, faces[0].x, faces[0].y, faces[0].x + faces[0].width, faces[0].y + faces[0].height);
-				cvtColor(face, face, COLOR_BGR2GRAY);
-				images.push_back(face);
-				labels.push_back(atoi(classlabel.c_str()));
-			}
-			else {
-				cout << "could not load image" << endl;
-				cout << path << endl;
+			//if (img.size() != size)
+			//resize(img, img, size);
+			Mat rImg;
+			if (!img.empty()) {
+				//cvtColor(img, img, COLOR_BGR2GRAY);
+				//equalizeHist(img, img);
+				face_cascade.detectMultiScale(img, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
+				if (faces.size() == 1) {
+					Mat face = copyFace(img, faces[0].x, faces[0].y, faces[0].x + faces[0].width, faces[0].y + faces[0].height);
+					cvtColor(face, face, COLOR_BGR2GRAY);
+					images.push_back(face);
+					labels.push_back(atoi(classlabel.c_str()));
+				}
+				else {
+					for (float i = 1; i < 11; i++) {
+						colorBalancing(img, rImg, i);
+						face_cascade.detectMultiScale(rImg, faces, 1.1, 2, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
+						if (faces.size() == 1) {
+							Mat face = copyFace(rImg, faces[0].x, faces[0].y, faces[0].x + faces[0].width, faces[0].y + faces[0].height);
+							cvtColor(face, face, COLOR_BGR2GRAY);
+							images.push_back(face);
+							labels.push_back(atoi(classlabel.c_str()));
+							i = 11;
+						}
+						else {
+							oneTime++;
+							if (oneTime == 10) {
+								cout << "could not load image" << endl;
+								cout << path << endl;
+								oneTime = 1;
+							}
+						}
+					}
+				}
+
+
 			}
 		}
 	}
